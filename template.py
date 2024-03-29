12c15079631088e1a46988eb800e07f33f4ae618
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-03-29 18:26:01 +0300

    project name change

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "wineQuality"
+project_name = "alzheimer"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
