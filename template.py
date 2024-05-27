7a33aab39b563b958189e9bf5f15141d6544919a
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-05-27 17:47:35 +0300

    Logger & few template changes

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='[%(levelname)s %(asctime)s %(filename)s]: %(message)s:')
 
-project_name = "potholeClassifier"
+project_name = "GenomeA"
 
 
 def create_directories(filepath: Path) -> None:
@@ -54,7 +54,7 @@ list_of_files = [
     f"src/{project_name}/components/model_training.py",
     f"src/{project_name}/components/model_evaluation.py",
     f"src/{project_name}/utils/_init__.py",
-    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/utils/utility_methods.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
@@ -64,7 +64,7 @@ list_of_files = [
     f"src/{project_name}/pipeline/stage_04_model_evaluation.py",
     f"src/{project_name}/pipeline/stage_05_prediction.py",
     f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/entity/entity_configurations.py",
     f"src/{project_name}/constants/__init__.py",
     f"src/{project_name}/logger/__init__.py",
     "config/config.yaml",
